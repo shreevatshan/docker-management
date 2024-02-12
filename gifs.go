358113c34a7605ce07beb72cb7daa26d74a0b566
shreevatshan <83694809+shreevatshan@users.noreply.github.com>
2024-02-12 17:56:50 +0530

    current dir

--- gifs.go
+++ gifs.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
 
 func loadingGIF(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("loading.gif"))
+	tmpl := template.Must(template.ParseFiles(filepath.Join(currentdir, "loading.gif")))
 	tmpl.Execute(w, nil)
 }
